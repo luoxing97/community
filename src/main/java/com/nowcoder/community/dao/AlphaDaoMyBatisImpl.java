150ed8535c0c10fdd1cd5d948990f191c458a043
luoxing97 <luoxing97@foxmail.com>
2024-06-10 12:00:48 +0800

    20240610提交

--- src/main/java/com/nowcoder/community/dao/AlphaDaoMyBatisImpl.java
+++ src/main/java/com/nowcoder/community/dao/AlphaDaoMyBatisImpl.java
@@ -16,6 +16,7 @@ public class AlphaDaoMyBatisImpl implements AlphaDao {
     }
 
     public static void main(String[] args) {
+        System.out.println("AlphaDaoMyBatisImpl");
         printAB();
     }
 
